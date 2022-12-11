03320018a8b62cf7d0e0cb6b1350cbab4df88fe8
kkoahis <khoakun27@gmail.com>
2022-12-11 15:33:30 +0700

    done API get User, get Product List, get Product Detail

---
+++ myproject/DjangoWeb/product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+# from .views import LatestProductList as views
+from . import views
+
+urlpatterns = [
+    path('latest-product/', views.LatestProductList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view())
+]
